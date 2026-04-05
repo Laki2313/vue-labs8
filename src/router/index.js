a957ee7ce08bb2791e25bcbaeffd146fc93c975e
Laki2313 <halych.ira@gmail.com>
2026-04-05 18:27:20 +0300

    Task 3: catalog + dynamic route /:id

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  },
   {
     path: '/catalog',
+    name: 'Catalog',
     component: () => import('../views/CatalogView.vue')
+  },
+  {
+    path: '/catalog/:id',
+    name: 'ProductDetails',
+    component: () => import('../views/ProductDetails.vue')
   }
 ]
 
